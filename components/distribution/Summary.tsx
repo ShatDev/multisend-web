b1e2adbb51ef99672703d2f2bf4ccbf1c8cc176c
vipin rawat <rawat.vipin4818@gmail.com>
2021-12-12 19:05:16 +0530

    added summary

--- components/distribution/Summary.tsx
+++ components/distribution/Summary.tsx
@@ -1,6 +1,73 @@
 const Summary = () => (
-  <div className="border-dashed border-2 border-yellow-500 lg:w-2/6 md:w-1/2 bg-yellow-100 rounded-2xl p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 min-h-screen">
+  <div className="border-dashed border-2 border-yellow-500 lg:w-2/6 md:w-1/2 bg-yellow-100 rounded-2xl p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 min-h-screen max-h-screen overflow-scroll">
     <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Summary</h2>
+
+    <div className="flex items-center justify-between mb-4">
+      <h2 className="text-gray-900 text-xl font-medium title-font mb-5">Token Address</h2>
+      <div className=" text-gray-400">Number of NFTs</div>
+    </div>
+
+    <div className="flex items-center justify-between mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">2</div>
+    </div>
+    <div className="flex items-center justify-between  mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">3</div>
+    </div>
+    <div className="flex items-center justify-between mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">2</div>
+    </div>
+    <div className="flex items-center justify-between  mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">3</div>
+    </div>
+    <div className="flex items-center justify-between mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">2</div>
+    </div>
+    <div className="flex items-center justify-between  mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">3</div>
+    </div>
+    <div className="flex items-center justify-between mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">2</div>
+    </div>
+    <div className="flex items-center justify-between  mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">3</div>
+    </div>
+    <div className="flex items-center justify-between mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">2</div>
+    </div>
+    <div className="flex items-center justify-between  mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">3</div>
+    </div>
+    <div className="flex items-center justify-between mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">2</div>
+    </div>
+    <div className="flex items-center justify-between  mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">3</div>
+    </div>
+    <div className="flex items-center justify-between mb-4">
+      <div className=" text-black">0x314ab97b76e39d63c78d5c86c2daf8eaa306b182</div>
+      <div className=" text-gray-400">2</div>
+    </div>
+
+    <div className="flex items-center justify-between mb-3">
+      <div className=" text-black text-xl font-bold">Balance</div>
+      <div className=" text-gray-400 text-xl">5</div>
+    </div>
+    <div className="flex items-center justify-between mb-3">
+      <div className=" text-black text-xl font-bold">Remaining</div>
+      <div className=" text-gray-400 text-xl">5</div>
+    </div>
   </div>
 );
 
