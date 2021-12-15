494bc0f034e80a30d95f0bdac3349f9529c71cfb
vipin rawat <rawat.vipin4818@gmail.com>
2021-12-15 18:10:55 +0530

    endpoint update

--- utils/config.ts
+++ utils/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  graphQlUri: 'https://multi-send-test.herokuapp.com/',
+  graphQlUri: 'https://multi-send-api.herokuapp.com/',
   multiSendContractAddress: '0x947D88a39cE8CdB213f5301D47891eCa1F82b4bC',
 };
 
