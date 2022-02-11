46eea183a8a10da69d6aaed8e9502baebeabdec4
devnasim <devnasim18@gmail.com>
2022-02-11 14:58:24 +0530

    minor fixes

--- utils/config.ts
+++ utils/config.ts
@@ -2,6 +2,11 @@ const config = {
   graphQlUri: 'https://multi-send-test.herokuapp.com/',
   // multiSendContractAddress: '0x947D88a39cE8CdB213f5301D47891eCa1F82b4bC',
   multiSendContractAddress: '0x30c9C76cdDd8086E3B2A1AcbfC16cd7F5d4Eec9C',
+  dropPerSlot: {
+    ERC20: 250,
+    ERC721: 100,
+    ERC1155: 100,
+  },
 };
 
 export default config;
