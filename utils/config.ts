b9fe024d72e9829dbf5d5952c1dcd23b93f9b7d8
D3G3N <42593114+d3g3n3rate3@users.noreply.github.com>
2022-03-11 10:37:29 +0100

    added my contract

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,7 @@
 const config = {
   graphQlUri: 'https://multi-send-test.herokuapp.com/',
   // multiSendContractAddress: '0x947D88a39cE8CdB213f5301D47891eCa1F82b4bC',
-  multiSendContractAddress: '0x49B3D22ed92BEFf6f796e06049b8C026404723e6',
+  multiSendContractAddress: '0x26AfD578B03AA7d76810F290F63991f5b89B925D',
   dropPerSlot: {
     ERC20: 250,
     ERC721: 100,
